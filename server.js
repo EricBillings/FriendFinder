09fdcbc462af18682c5c3ade53e28e2ecde37893
Eric Billings <billings.company@gmail.com>
2019-08-19 09:05:02 -0400

    setup server

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const path = require("path");
 const express = require("express");
+const PORT = 3000;
+
+let app = express();
+
+
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+  })
+   
+  app.listen(PORT, function () {
+      console.log(`Listening on http://localhost:${PORT}`);
+  })
