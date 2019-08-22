2fc1c04f12e1f40e9757d7e28d831fdee09a0cc6
Eric Billings <billings.company@gmail.com>
2019-08-21 21:50:28 -0400

    added basic styling and html navigation

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const PORT = 3000;
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static( path.join( __dirname, "./app/public" ) ));
 
 require("./routing/apiRoutes")(app);
 require("./routing/htmlRoutes")(app);
